e49ce2354caebe2a2816e458c5c8a75da80e5f21
yuristrelets <yuristrelets@gmail.com>
2015-03-28 15:14:46 +0200

    add models

---
+++ app/models/HelpType.php
@@ -0,0 +1,12 @@
+<?php
+
+class HelpType extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'help_types';
+
+}
---
+++ app/models/ProjectHelper.php
@@ -0,0 +1,12 @@
+<?php
+
+class ProjectHelper extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'project_helpers';
+
+}
