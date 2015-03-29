20ee8aab842598979b35bfb2240c658a4797f6e3
Alexey Lysenko <nasguling@gmail.com>
2015-03-29 16:39:41 +0300

    add project controller 2 routes

--- app/controllers/ProjectController.php
+++ app/controllers/ProjectController.php
@@ -9,8 +9,31 @@ class ProjectController extends \BaseController {
 	 */
 	public function index()
 	{
-		//
-	}
+        try{
+            $statusCode = 200;
+            $response = [
+                'projects'  => []
+            ];
+
+            $projects = Project::where('is_draft',0)->where('is_closed',0)->get();
+
+            foreach($projects as $project){
+                $response['projects'][] = [
+                    'id' => $project->id,
+                    'title' => $project->title,
+                    'description' => $project->description,
+                    'text' => $project->text,
+                    'created_at' => $project->created_at,
+                    'updated_at' => $project->updated_at,
+                ];
+            }
+
+        }catch (Exception $e){
+            $statusCode = 400;
+        } finally {
+            return Response::make($response, $statusCode);;
+        }
+    }
 
 
 	/**
@@ -43,8 +66,29 @@ class ProjectController extends \BaseController {
 	 */
 	public function show($id)
 	{
-		//
-	}
+        try{
+            $statusCode = 200;
+            $response = [
+                'projects'  => []
+            ];
+
+            $project = Project::where('id',$id)->firstOrFail();
+
+            $response['projects'][] = [
+                'id' => $project->id,
+                'title' => $project->title,
+                'description' => $project->description,
+                'text' => $project->text,
+                'created_at' => $project->created_at,
+                'updated_at' => $project->updated_at,
+            ];
+
+        }catch (Exception $e){
+            $statusCode = 400;
+        } finally {
+            return Response::make($response, $statusCode);;
+        }
+    }
 
 
 	/**
--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,7 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::group(['prefix' => 'api'], function() {
+    Route::resource('projects', 'ProjectController');
+});
