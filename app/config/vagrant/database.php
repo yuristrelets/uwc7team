facda1954a49caeee5144456fd8ca523b0b49439
Alexey Lysenko <nasguling@gmail.com>
2015-03-29 16:40:53 +0300

    return right config for vagrant

--- app/config/vagrant/database.php
+++ app/config/vagrant/database.php
@@ -22,10 +22,10 @@ return array(
 
         'mysql' => array(
             'driver'    => 'mysql',
-            'host'      => 'localhost',
-            'database'  => 'beardshop',
-            'username'  => 'dbuser',
-            'password'  => '123',
+            'host'      => getenv('DB_HOST'),
+            'database'  => getenv('DB_NAME'),
+            'username'  => getenv('DB_USER'),
+            'password'  => getenv('DB_PASS'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
